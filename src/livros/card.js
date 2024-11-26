be500f19bad7784e7762ac150bee3a20869aca18
David Mizrahy Neto <160736321+DavidMzNeto@users.noreply.github.com>
2024-11-25 21:49:55 -0300

    Update card.js

--- src/livros/card.js
+++ src/livros/card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import {
   MDBCard,
@@ -7,21 +8,27 @@ import {
   MDBCardText,
   MDBCardImage,
   MDBBtn,
+  MDBRow,
 } from "mdb-react-ui-kit";
 
 export function CardLivros({ id, thumbnail, title, authors, Mover }) {
   return (
-    <MDBCard>
+    <MDBCard className="my-3">
       <MDBCardImage
         src={thumbnail}
         alt="Capa do Livro"
         position="top"
-
+        style={{ height: "200px", objectFit: "cover" }}
       />
       <MDBCardBody>
         <MDBCardTitle>{title}</MDBCardTitle>
         <MDBCardText>{authors}</MDBCardText>
-        <MDBBtn onClick={() => Mover(id)}>Mover Livro</MDBBtn>
+        <MDBRow className="d-flex justify-content-between">
+          <MDBBtn onClick={() => Mover(id)}>Mover Livro</MDBBtn>
+          <MDBBtn tag={Link} to={`/livros/${id}`} color="info">
+            Ver Detalhes
+          </MDBBtn>
+        </MDBRow>
       </MDBCardBody>
     </MDBCard>
   );
